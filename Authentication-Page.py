3624f71a6f737cb79a01cd37dd72b9cf1a83fc13
Parsamehr007 <parsamehrbolandnazar@gmail.com>
2024-09-09 18:22:08 +0330

    Implementation of password hashing

--- Authentication-Page.py
+++ Authentication-Page.py
@@ -2,7 +2,7 @@ import os
 import time 
 import re
 import json
-
+import bcrypt
 
 data_file = "data.json"
 if not os.path.exists(data_file):
@@ -17,11 +17,21 @@ def update_file(new_data):
     with open(data_file , 'w') as f:
         json.dump(new_data , f)
 
+
+def hash_password(password):
+
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed_password
+
+def check_password(password , hashed):
+    return bcrypt.checkpw(password.encode("utf-8"), hashed)
+
 def login():
     data = load_file()
     username = input("username : ")
     password = input("password : ")
-    if username in data and password == data[username] :
+    if username in data and check_password(password, data[username].encode("utf-8")) :
         print("loging in")
     else:
         print("username or password are incorrect")
@@ -50,7 +60,8 @@ def signup():
         if username in data :
             print(" username is Duplicate")
             return
-        data[username] = password
+        hsd_password = hash_password(password)
+        data[username] = hsd_password.decode("utf-8")
         print("sign up successfully")
         update_file(data)
     else :
@@ -58,7 +69,7 @@ def signup():
 
 def main():
     while True :
-        os.system("cls")
+        # os.system("cls")          #for windows terminal
         choose = input(" 1 : login \n 2 : sign up \n 3 : exit \n choose :")
         if choose ==  "1" :
             login()
